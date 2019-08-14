586d3d0db1a35c70da5bd02d4db383da28b1ff54
Jake Shilling <shilling.jake@gmail.com>
2019-08-14 09:27:40 -0400

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,12 @@
-def hello_t
+# frozen_string_literal: true
 
+def hello_t(array)
+  if block_given?
+    array.each { |el| yield el }
+  else
+    puts 'Hey! No block was given!'
+  end
 end
 
-# call your method here!
+#Methodyour method here!
 
