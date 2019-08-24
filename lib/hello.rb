68037ef097ac2c14977cfa9ab249ab2985e7657f
Kevin Hayden <kevin.fhayden@gmail.com>
2019-08-24 21:54:52 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,23 @@
-def hello_t
-
+def hello_t(array)
+  if block_given?
+    i = 0 #counter variable for while loop
+  
+   while i < array.length #while counter is less than array.length
+      yield array[i] #use bracket[] to grab each index element
+     i = i + 1 #counting up with the counter
+    end
+    
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
+hello_t(["Tim", "Tom", "Jim"]) do
+  |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
 # call your method here!
 
